79b105fbb003b872b43bbe10643d24d970b51575
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-26 13:06:16 +0530

    Added : Add ability to create and display a new contact

--- src/AddressBookMain.java
+++ src/AddressBookMain.java
@@ -1,5 +1,6 @@
+import java.util.Scanner;
 
- class ContactPerson {
+class ContactPerson {
     private String firstName;
     private String lastName;
     private String address;
@@ -60,8 +61,30 @@
 public class AddressBookMain {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome To Address Book");
+
         AddressBook addressBook = new AddressBook();
+
+        System.out.println("Enter First Name:");
+        String firstName = scanner.nextLine();
+        System.out.println("Enter Last Name:");
+        String lastName = scanner.nextLine();
+        System.out.println("Enter Address:");
+        String address = scanner.nextLine();
+        System.out.println("Enter City:");
+        String city = scanner.nextLine();
+        System.out.println("Enter State:");
+        String state = scanner.nextLine();
+        System.out.println("Enter Zip:");
+        String zip = scanner.nextLine();
+        System.out.println("Enter Phone Number:");
+        String phoneNumber = scanner.nextLine();
+        System.out.println("Enter Email:");
+        String email = scanner.nextLine();
+
+        ContactPerson newContact = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
         addressBook.showContacts();
     }
 }
